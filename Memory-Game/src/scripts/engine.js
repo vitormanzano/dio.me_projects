d1fe1f0ed475a5d4b86006b035a78c6473a60db6
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2024-11-05 20:29:08 -0300

    Memory Game

---
+++ Memory-Game/src/scripts/engine.js
@@ -0,0 +1,61 @@
+const emojis = [
+    '😸',
+    '😸',
+    '🐶',
+    '🐶',
+    '🐵',
+    '🐵',
+    '🦁',
+    '🦁',
+    '🐯',
+    '🐯',
+    '🐮',
+    '🐮',
+    '🐰',
+    '🐰',
+    '🦊',
+    '🦊'
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for ( let i = 0; i<emojis.length; i++){
+    let box = document.createElement("div");
+    box.className = "item";
+    box.onclick = handleClick;
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box)
+}
+
+function handleClick(){
+    if ( openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if ( openCards.length == 2){
+        
+        setTimeout(checkMatch, 500);
+
+    }
+}
+
+function checkMatch(){
+    if ( openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch");
+        openCards[0].classList.add("boxMatch");
+
+    }
+    else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+        
+    }
+    openCards = [];
+    console.log(document.querySelectorAll(".boxMatch").length);
+
+    if (document.querySelectorAll(".boxMatch").length === emojis.length){
+        alert("Você Venceu!");
+    }
+}
